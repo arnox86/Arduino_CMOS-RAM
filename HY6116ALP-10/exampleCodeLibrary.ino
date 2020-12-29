363640a67d6772e7f10868b75939916b6d531bf6
Bene <63281428+arnox86@users.noreply.github.com>
2020-12-29 16:18:37 +0100

    Update exampleCodeLibrary.ino

--- HY6116ALP-10/exampleCodeLibrary.ino
+++ HY6116ALP-10/exampleCodeLibrary.ino
@@ -27,3 +27,30 @@ SOFTWARE.
 /*
 *
 */
+
+#define Ad0 47    // Adress pins (example pins)
+#define Ad1 45
+#define Ad2 43
+#define Ad3 41
+#define Ad4 39
+#define Ad5 37
+#define Ad6 35
+#define Ad7 33
+#define Ad8 32
+#define Ad9 34
+#define Ad10 40
+
+#define IO1 49    // Input- / Output- pins (example pins)
+#define IO2 51
+#define IO3 53
+#define IO4 52
+#define IO5 50
+#define IO6 48
+#define IO7 46
+#define IO8 44
+
+#define OE 38    // Output Enable / Write Enable (example pins)
+#define WE 36
+
+#define maxSize 0x07FF    // Maximum Size of memory (example pins)
+#define maxSizeInv 0xFFFF-maxSize
